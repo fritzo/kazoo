4f18218cbf04239f34169b87b2d8f373594b28e9
Fritz Obermeyer <fritz@priorknowledge.com>
2012-12-15 14:55:36 -0800

    Add test for gcd

--- src/rational_test.cpp
+++ src/rational_test.cpp
@@ -1,8 +1,58 @@
 
 #include "rational.h"
 
+void test_gcd ()
+{
+  using namespace Rational;
+
+  LOG("Testing gcd");
+
+  ASSERT_EQ(gcd(1,1), 1);
+  ASSERT_EQ(gcd(1,2), 1);
+  ASSERT_EQ(gcd(1,3), 1);
+  ASSERT_EQ(gcd(1,4), 1);
+  ASSERT_EQ(gcd(1,5), 1);
+  ASSERT_EQ(gcd(1,6), 1);
+
+  ASSERT_EQ(gcd(2,1), 1);
+  ASSERT_EQ(gcd(2,2), 2);
+  ASSERT_EQ(gcd(2,3), 1);
+  ASSERT_EQ(gcd(2,4), 2);
+  ASSERT_EQ(gcd(2,5), 1);
+  ASSERT_EQ(gcd(2,6), 2);
+
+  ASSERT_EQ(gcd(3,1), 1);
+  ASSERT_EQ(gcd(3,2), 1);
+  ASSERT_EQ(gcd(3,3), 3);
+  ASSERT_EQ(gcd(3,4), 1);
+  ASSERT_EQ(gcd(3,5), 1);
+  ASSERT_EQ(gcd(3,6), 3);
+
+  ASSERT_EQ(gcd(4,1), 1);
+  ASSERT_EQ(gcd(4,2), 2);
+  ASSERT_EQ(gcd(4,3), 1);
+  ASSERT_EQ(gcd(4,4), 4);
+  ASSERT_EQ(gcd(4,5), 1);
+  ASSERT_EQ(gcd(4,6), 2);
+
+  ASSERT_EQ(gcd(5,1), 1);
+  ASSERT_EQ(gcd(5,2), 1);
+  ASSERT_EQ(gcd(5,3), 1);
+  ASSERT_EQ(gcd(5,4), 1);
+  ASSERT_EQ(gcd(5,5), 5);
+  ASSERT_EQ(gcd(5,6), 1);
+
+  ASSERT_EQ(gcd(6,1), 1);
+  ASSERT_EQ(gcd(6,2), 2);
+  ASSERT_EQ(gcd(6,3), 3);
+  ASSERT_EQ(gcd(6,4), 2);
+  ASSERT_EQ(gcd(6,5), 1);
+  ASSERT_EQ(gcd(6,6), 6);
+}
+
 void test_ball ()
 {
+  LOG("Testing ball");
   auto ball = Rational::ball_of_radius(4);
   ASSERT_EQ(ball.size(), 7);
   ASSERT_EQ(ball[0], Rational::Number(1, 3));
@@ -16,6 +66,7 @@ void test_ball ()
 
 int main ()
 {
+  test_gcd();
   test_ball();
 
   return 0;
